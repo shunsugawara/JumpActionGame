7e699721326cee90586438e4222b37ff652370a8
shunsugawara <p0921065@gmail.com>
2018-06-25 15:07:16 +0900

    Initial commit(課題取組前）

---
+++ core/src/jp/techacademy/shun/sugawara/jumpactiongame/Ufo.java
@@ -0,0 +1,13 @@
+package jp.techacademy.shun.sugawara.jumpactiongame;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Ufo extends GameObject {
+    public static final float UFO_WIDTH = 2.0f;
+    public static final float UFO_HEIGHT = 1.3f;
+
+    public Ufo(Texture texture,int srcX,int srcY ,int srcWidth , int srcHeight){
+        super(texture,srcX,srcY,srcWidth,srcHeight);
+        setSize(UFO_WIDTH,UFO_HEIGHT);
+    }
+}
